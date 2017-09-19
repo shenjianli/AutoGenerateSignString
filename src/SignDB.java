ec278a10da19cd98917f5b04a3d9b307bd6ee176
shenjiali <cqtddt@163.com>
2017-09-19 16:12:32 +0800

    commit the sign database

--- src/SignDB.java
+++ src/SignDB.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SignDB {
+	
 	Connection conn;
 
 	public boolean openDBConnection() {
@@ -25,6 +26,7 @@ public class SignDB {
 
 		try {
 			conn = DriverManager.getConnection(url, "root", "cqtddt@2016");
+			System.out.println("建立mysql链接成功");
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -37,19 +39,24 @@ public class SignDB {
 		List<String> result = new ArrayList<String>();
 		
 		try {
+			System.out.println("开始查询-----用户数据");
 			Statement stmt;
 			stmt = conn.createStatement();
-
+			
 			String sql = "select * from user"; // 要执行的SQL
 			ResultSet rs = stmt.executeQuery(sql);// 创建数据对象
 			while (rs.next()) {
 				System.out.print(rs.getInt(1) + "\t");
 				System.out.print(rs.getString(2) + "\t");
 				System.out.print(rs.getString(3) + "\t");
-				System.out.print(rs.getString(8) + "\t");
+				System.out.print(rs.getString(4) + "\t");
 				System.out.println();
 				result.add(rs.getString(4));
 			}
+			rs.close();
+			stmt.close();
+			System.out.println("结束查询-----用户数据" + result.size());
+			System.out.println();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -57,90 +64,106 @@ public class SignDB {
 		return result;
 	}
 
-	
-	public boolean updateSignKey(String employeeId){
-		String sql2 = "update user set sign_key=? where employee_id=?";
-		PreparedStatement pst;
+	public void closeConn() {
 		try {
-			pst = conn.prepareStatement(sql2);
-			pst.setString(1, "shenjianlikey");
-			pst.setString(2, "00000185");
-			int result = pst.executeUpdate();
-			if(result > 0){
-				return true;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-	public static void main(String[] args) {
-		try {
-			// 调用Class.forName()方法加载驱动程序
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("成功加载MySQL驱动！");
-		} catch (ClassNotFoundException e1) {
-			System.out.println("找不到MySQL驱动!");
-			e1.printStackTrace();
-		}
-
-		String url = "jdbc:mysql://localhost:3306/sign"; // JDBC的URL
-		// 调用DriverManager对象的getConnection()方法，获得一个Connection对象
-		Connection conn;
-		try {
-			conn = DriverManager.getConnection(url, "root", "cqtddt@2016");
-			// 创建一个Statement对象
-			Statement stmt = conn.createStatement(); // 创建Statement对象
-			System.out.println("成功连接到数据库！");
-
-			String sql = "select * from user"; // 要执行的SQL
-			ResultSet rs = stmt.executeQuery(sql);// 创建数据对象
-			System.out.println("名字" + "\t" + "姓氏" + "\t" + "年龄");
-			while (rs.next()) {
-				System.out.print(rs.getInt(1) + "\t");
-				System.out.print(rs.getString(2) + "\t");
-				System.out.print(rs.getString(3) + "\t");
-				System.out.print(rs.getString(8) + "\t");
-				System.out.println();
-			}
-
-			// String insql="insert into
-			// employee(FIRST_NAME,LAST_NAME,AGE,SEX,INCOME) values(?,?,?,?,?)";
-			// PreparedStatement psInsert = conn.prepareStatement(insql);
-			// psInsert.setString(1,"jerry");
-			// psInsert.setString(2,"shen");
-			// psInsert.setInt(3,30);
-			// psInsert.setString(4,"M");
-			// psInsert.setInt(5,50000);
-			// int result = psInsert.executeUpdate();
-			// if(result > 0){
-			// System.out.println("插入数据成功");
-			// }
-			// else {
-			// System.out.println("插入数据失败");
-			// }
-
-			// 修改数据的代码
-			String sql2 = "update user set sign_key=? where employee_id=?";
-			PreparedStatement pst = conn.prepareStatement(sql2);
-			pst.setString(1, "shenjianlikey");
-			pst.setString(2, "00000185");
-			pst.executeUpdate();
-
-			// 删除数据的代码
-			// String sql3 = "delete from stu where number=?";
-			// pst = conn.prepareStatement(sql3);
-			// pst.setInt(1,701);
-			// pst.executeUpdate();
-
-			rs.close();
-			stmt.close();
 			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 	}
+	
+	public boolean updateSignKey(String employeeId,String signKey,String signOutKey){
+		String sql2 = "update user set sign_key=?,sign_out_key=? where employee_id=?";
+		PreparedStatement pst;
+		try {
+			System.out.println(employeeId + "开始更新-----加密串");
+			pst = conn.prepareStatement(sql2);
+			pst.setString(1, signKey);
+			pst.setString(2, signOutKey);
+			pst.setString(3, employeeId);
+			
+			int result = pst.executeUpdate();
+			
+			pst.close();
+			
+			if(result > 0){
+				return true;
+			}
+			System.out.println(employeeId + "完成更新-----加密串");
+			System.out.println();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	
+//	public static void main(String[] args) {
+//		try {
+//			// 调用Class.forName()方法加载驱动程序
+//			Class.forName("com.mysql.jdbc.Driver");
+//			System.out.println("成功加载MySQL驱动！");
+//		} catch (ClassNotFoundException e1) {
+//			System.out.println("找不到MySQL驱动!");
+//			e1.printStackTrace();
+//		}
+//
+//		String url = "jdbc:mysql://localhost:3306/sign"; // JDBC的URL
+//		// 调用DriverManager对象的getConnection()方法，获得一个Connection对象
+//		Connection conn;
+//		try {
+//			conn = DriverManager.getConnection(url, "root", "cqtddt@2016");
+//			// 创建一个Statement对象
+//			Statement stmt = conn.createStatement(); // 创建Statement对象
+//			System.out.println("成功连接到数据库！");
+//
+//			String sql = "select * from user"; // 要执行的SQL
+//			ResultSet rs = stmt.executeQuery(sql);// 创建数据对象
+//			System.out.println("名字" + "\t" + "姓氏" + "\t" + "年龄");
+//			while (rs.next()) {
+//				System.out.print(rs.getInt(1) + "\t");
+//				System.out.print(rs.getString(2) + "\t");
+//				System.out.print(rs.getString(3) + "\t");
+//				System.out.print(rs.getString(8) + "\t");
+//				System.out.println();
+//			}
+//
+//			// String insql="insert into
+//			// employee(FIRST_NAME,LAST_NAME,AGE,SEX,INCOME) values(?,?,?,?,?)";
+//			// PreparedStatement psInsert = conn.prepareStatement(insql);
+//			// psInsert.setString(1,"jerry");
+//			// psInsert.setString(2,"shen");
+//			// psInsert.setInt(3,30);
+//			// psInsert.setString(4,"M");
+//			// psInsert.setInt(5,50000);
+//			// int result = psInsert.executeUpdate();
+//			// if(result > 0){
+//			// System.out.println("插入数据成功");
+//			// }
+//			// else {
+//			// System.out.println("插入数据失败");
+//			// }
+//
+//			// 修改数据的代码
+//			String sql2 = "update user set sign_key=? where employee_id=?";
+//			PreparedStatement pst = conn.prepareStatement(sql2);
+//			pst.setString(1, "shenjianlikey");
+//			pst.setString(2, "00000185");
+//			pst.executeUpdate();
+//
+//			// 删除数据的代码
+//			// String sql3 = "delete from stu where number=?";
+//			// pst = conn.prepareStatement(sql3);
+//			// pst.setInt(1,701);
+//			// pst.executeUpdate();
+//
+//			rs.close();
+//			stmt.close();
+//			conn.close();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
 
 }
--- src/SignTask.java
+++ src/SignTask.java
@@ -3,6 +3,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TimerTask;
 
@@ -11,11 +12,9 @@ public class SignTask extends TimerTask {
 	@Override
 	public void run() {
 		String currentdate = "";
-		String employeeId = "00000185";
-		Calendar now = Calendar.getInstance();
 		
+		Calendar now = Calendar.getInstance();
 		now = Calendar.getInstance();
-
 		System.out.println("年: " + now.get(Calendar.YEAR));
 		System.out.println("月: " + (now.get(Calendar.MONTH) + 1) + "");
 		System.out.println("日: " + now.get(Calendar.DAY_OF_MONTH));
@@ -31,6 +30,9 @@ public class SignTask extends TimerTask {
 		// format date pattern
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 		currentdate = formatter.format(nowdate);
+		String employeeId = "00000185";
+		
+		
 
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("currentdate", currentdate);
@@ -68,9 +70,57 @@ public class SignTask extends TimerTask {
 		}
 		if (WriteStringToTxt.WriteStringToFile("./signout.txt", signOut)) {
 			System.out.println("写入签退加密字符串文件成功");
-			System.out.print(">>");
 		}
-		System.out.println("自动产生签到，签退，加密串完成");
+		System.out.println("");
+		
+		SignDB signDB = new SignDB();
+		if(signDB.openDBConnection()) {
+			
+			List<String> userIds = signDB.getUserId();
+			System.out.println("开始产生------加密字符串");
+			for(String userId : userIds ) {
+				
+				Map<String, String> signParam = new HashMap<String, String>();
+				signParam.put("currentdate", currentdate);
+				signParam.put("employeeId", userId);
+				signParam.put("inOutFlag", "0");
+
+				String signKey = null;
+				String signInKeyStr = EncryptUtils.getParameterStr(signParam);
+				try {
+					signKey = EncryptUtils.encode(signInKeyStr);
+					System.out.println(employeeId + "生成---签到字符串:" + signKey);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				
+				Map<String, String> signOutMap = new HashMap<String, String>();
+				signOutMap.put("currentdate", currentdate);
+				signOutMap.put("employeeId", userId);
+				signOutMap.put("inOutFlag", "1");
+				String signOutKey = null;
+				String signOutKeyStr = EncryptUtils.getParameterStr(signOutMap);
+				try {
+					signOutKey = EncryptUtils.encode(signOutKeyStr);
+
+					System.out.println(employeeId + "生成---签退字符串:" + signOutKey);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				boolean updateResult = signDB.updateSignKey(userId,signKey,signOutKey);
+				if(updateResult) {
+					System.out.println(employeeId + "更新----签到签退加密字符串------成功");
+				}
+				System.out.println();
+			}
+			System.out.println("完成产生------加密字符串");
+		}
+		signDB.closeConn();
+		
+		System.out.println("自动产生------签到，签退，加密串-----完成");
 	}
 
 }
